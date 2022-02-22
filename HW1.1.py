28e6720f4cc5fbc468d26c718c6a7d78a09c1e5a
ypooja11 <47106500+ypooja11@users.noreply.github.com>
2022-02-22 01:14:19 -0500

    Add files via upload

---
+++ HW1.1.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(784, 256)
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(256, 10)
+        
+        # Define sigmoid activation and softmax output 
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+        
+    def forward(self, x):
+        # Pass the input tensor through each of our operations
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        x = self.softmax(x)
+        
+        return x
+
+        class Network(nn.Module):
+            self.hidden = nn.Linear(784, 256)
+            self.output = nn.Linear(256, 10)
+            self.sigmoid = nn.Sigmoid()
+            self.softmax = nn.Softmax(dim=1)
+    
+def forward(self, x):
+    x = self.hidden(x)
+    x = self.sigmoid(x)
+    x = self.output(x)
+    x = self.softmax(x)
+model = Network()
+model
+
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10# Build a feed-forward network
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.Softmax(dim=1))
+print(model)
+
+from collections import OrderedDict
+model = nn.Sequential(OrderedDict([
+                      ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                      ('relu1', nn.ReLU()),
+                      ('fc2', nn.Linear(hidden_sizes[0], hidden_sizes[1])),
+                      ('relu2', nn.ReLU()),
+                      ('output', nn.Linear(hidden_sizes[1], output_size)),
+                      ('softmax', nn.Softmax(dim=1))]))
+model
+
